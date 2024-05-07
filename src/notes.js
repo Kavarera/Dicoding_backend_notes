a0876a8469576275427bed641a6088fafdbbea90
Kavarera <rafli.030715@gmail.com>
2024-05-07 17:14:25 +0700

    change little bit

--- src/notes.js
+++ src/notes.js
@@ -1,4 +1,8 @@
-const notes =[]
+const notes =[];
+const newNote = {
+        "title", "tags", "body", 1, Date.now().toString(), Date.now().toString(),
+      };
+notes.push(newNote);
 
 
 module.exports=notes;
